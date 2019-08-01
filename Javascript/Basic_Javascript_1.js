59b86c4301e3fee3ad3c72472f5120df9b1630e7
LandoCalrissian2 <davislandon33@gmail.com>
2019-08-01 08:58:36 -0500

    First submission
    First submission

---
+++ Javascript/Basic_Javascript_1.js
@@ -0,0 +1,20 @@
+window.alert("Hello World");
+
+var hi="hi";
+
+var b="\`"
+
+
+var letters = first="1", second="2",third="3";
+
+var first = first.fontcolor("blue");
+
+document.write(first);
+
+document.write(b);
+
+console.log(hi+b);
+
+document.write(second);
+
+document.write(3+3);
