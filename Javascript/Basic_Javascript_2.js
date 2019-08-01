aaf32c69a4a36f236ab20833483e11a154e6142d
LandoCalrissian2 <davislandon33@gmail.com>
2019-08-01 09:24:14 -0500

    assignment submission #2
    assignment submission #2

---
+++ Javascript/Basic_Javascript_2.js
@@ -0,0 +1,10 @@
+var a="hello";
+var b="world";
+
+function myFunction() {
+    document.getElementById("btn").innerHTML="blue";
+  }
+
+  function myFunction() {
+    document.getElementById("btn").innerHTML = a+b;
+  }
