3dbb92b37c31c9da9f13c50f62b8c0e181ba23ee
LandoCalrissian2 <davislandon33@gmail.com>
2019-08-02 10:43:01 -0500

    submission 8
    submission 8

---
+++ Javascript/Basic_Javascript_8.js
@@ -0,0 +1,11 @@
+var a ="111";
+var b ="222";
+var c = 333;
+var d = 1.5843259723489057234957;
+console.log(a.concat(a,b));
+
+console.log(a.slice(2));
+
+console.log(c.toString());
+
+console.log(d.toPrecision(5));
